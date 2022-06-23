72d5cc43ac69476d7d00c7c0e8f02ad0d2cca407
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-06-23 17:23:31 +0300

    Додає рішення 8 задачі

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onHandleSubmit);
+
+function onHandleSubmit(event) {
+    event.preventDefault();
+    
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert('Заповніть будь-ласка всі поля!')
+    }
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(data);
+    
+   event.currentTarget.reset();
+}
