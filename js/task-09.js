c5778917c330d3e57796c654f7f45acc6322b800
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-06-23 17:55:06 +0300

    Додає рішення 9 задачі

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorBody = document.querySelector('.color');
+
+buttonEl.addEventListener('click', () => {
+bodyEl.style.backgroundColor = getRandomHexColor();
+colorBody.textContent = getRandomHexColor();
+})
