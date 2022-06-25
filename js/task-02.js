ac092c4356b2ce0d716bd2c9cad6ef6fbe3a4ed9
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-06-25 20:08:02 +0300

    Додає зміни в 2 задачу

--- js/task-02.js
+++ js/task-02.js
@@ -7,32 +7,10 @@ const ingredients = [
   'Condiments',
 ];
 
-const itemsPotatoes = document.createElement('li');
-itemsPotatoes.classList.add('item');
-itemsPotatoes.textContent = 'Potatoes';
+ const listEl = document.querySelector('#ingredients');
+ const ingredientItem = ingredients.map(ingredient => {
+  const items = document.createElement('li');
+  items.textContent = ingredient;
+  listEl.appendChild(items);
+})
 
-const itemsMushrooms = document.createElement('li');
-itemsMushrooms.classList.add('item');
-itemsMushrooms.textContent = 'Mushrooms';
-
-const itemsGarlic = document.createElement('li');
-itemsGarlic.classList.add('item');
-itemsGarlic.textContent = 'Garlic';
-
-const itemsTomatos = document.createElement('li');
-itemsTomatos.classList.add('item');
-itemsTomatos.textContent = 'Tomatos';
-
-const itemsHerbs = document.createElement('li');
-itemsHerbs.classList.add('item');
-itemsHerbs.textContent = 'Herbs';
-
-const itemsCondiments = document.createElement('li');
-itemsCondiments.classList.add('item');
-itemsCondiments.textContent = 'Condiments';
-
-const listEl = document.querySelector('#ingredients');
-
-listEl.append(itemsPotatoes, itemsMushrooms, itemsGarlic, itemsTomatos, itemsHerbs, itemsCondiments);
-
-console.log(listEl);
