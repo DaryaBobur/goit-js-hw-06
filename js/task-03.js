77559685de24dd53a116c239c79d087c987e747a
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-06-25 20:09:17 +0300

    Додає зміни в класи 3 задачі

--- js/task-03.js
+++ js/task-03.js
@@ -15,7 +15,7 @@ const images = [
 
 const galleryEl = document.querySelector('.gallery');
 
-const imagesGallery = images.map((image) => `<li class = "item"><img class = "img" src = ${ image.url } alt = "${ image.alt }"></li>`).join('');
+const imagesGallery = images.map((image) => `<li class = "gallery__item"><img class = "gallery__img" src = ${ image.url } alt = "${ image.alt }"></li>`).join('');
 
 galleryEl.insertAdjacentHTML('beforeend', imagesGallery)
 
