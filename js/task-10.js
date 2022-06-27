3b3b108f6d3b162934e6249dc86b91675ddd2d93
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-06-27 20:01:30 +0300

    Додає правки до 10 задачі

--- js/task-10.js
+++ js/task-10.js
@@ -1,7 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
-
 const boxesEl = document.querySelector('#boxes');
 
 const inputEl = document.querySelector('input');
@@ -10,29 +6,32 @@ const createBtn = document.querySelector('button[data-create]');
 
 const destroyBtn = document.querySelector('button[data-destroy]')
 
-
 createBtn.addEventListener('click', createBoxes);
 destroyBtn.addEventListener('click', destroyBoxes);
 
-function createBoxes(amount) {
-  amount = inputEl.value;
-  const firstSizeDiv = 30;
-  let fragment = document.createDocumentFragment();
+let firstSizeDiv = 20;
 
+function createBoxes() {
+  const amount = inputEl.value;
+  const fragment = document.createDocumentFragment(); 
   for (let i = 0; i < amount; i += 1) {
     const newDiv = document.createElement('div');
-    const sizeNewDiv = firstSizeDiv + i * 10 + 'px';
-   
-    newDiv.style.width = sizeNewDiv;
-    newDiv.style.height = sizeNewDiv;
+  
+    firstSizeDiv += 10;
+    newDiv.style.width = firstSizeDiv + 'px';
+    newDiv.style.height = firstSizeDiv + 'px';
     newDiv.style.backgroundColor = getRandomHexColor();
     newDiv.style.marginTop = '5px'
 
-    fragment.appendChild(newDiv);
+  fragment.append(newDiv);
 }
-  boxesEl.appendChild(fragment);
+ boxesEl.append(fragment);
 }
 
 function destroyBoxes() {
   boxesEl.textContent = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
